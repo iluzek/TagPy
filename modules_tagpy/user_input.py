32e0944b13e809f5fac01c0c76276b331fc99c20
iluzek <iluzek@users.noreply.github.com>
2016-11-20 01:39:52 +0000

    Update user_input.py
    Removes the bug where path encapsulated in " "  left last " in the string. 
    String could not pass as valid directory when that happened.

--- modules_tagpy/user_input.py
+++ modules_tagpy/user_input.py
@@ -7,6 +7,10 @@ import argparse
 # =============================================================================
 def validate_path(path_string):
     import os
+    # problem is that when path is encapsulated with " " , argument gets passed but last " stays in path
+    if path_string.endswith("\""):  # if string contains " as last character
+        path_string = path_string[:-1] # remove last character
+    # proceed as before
     mod_path_string = os.path.join(path_string, '')
     if os.path.isdir(mod_path_string):
         # print(mod_path_string)
